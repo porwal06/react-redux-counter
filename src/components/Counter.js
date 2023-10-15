f1525215a48abe1f651b6caf98945c75420b3ef0
Deepak Porwal <deepak.porwal@impelsys.com>
2023-10-15 17:00:06 +0530

    Add increment & decrement by 10 or any custom number

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,7 +4,10 @@ import classes from './Counter.module.css';
 const Counter = () => {
   const dispatch = useDispatch();
   const counterNumber = useSelector(store => store.counter);
-  const toggleCounterHandler = () => {};
+  const showToggle = useSelector(store => store.show);
+  const toggleCounterHandler = () => {
+    dispatch({type: 'toggle'});
+  };
 
   const incrementCounterHandler = () => {
     dispatch({type: 'increment'});
@@ -12,14 +15,20 @@ const Counter = () => {
   const descrementCounterHandler = () => {
     dispatch({type: 'decrement'});
   };
+  const counterHandler = (event, opr = 'increase', val=5) => {
+    dispatch({type: 'counter', op:opr, value: val});
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counterNumber}</div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+      {showToggle &&<div className={classes.value}>{counterNumber}</div>}
       <button onClick={incrementCounterHandler}>Increment Counter</button>
       <button onClick={descrementCounterHandler}>Descrement Counter</button>
+      <button onClick={event => counterHandler(event, 'increase', 10)}>Increase Counter by 10</button>
+      <button onClick={event => counterHandler(event, 'descrese', 10)}>Descrese Counter by 10</button>
+
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
 };
